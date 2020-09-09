57b3f6a575213c8b909448ff22e80b844d5d2c9b
Max <addergtx@gmail.com>
2020-09-09 20:49:26 +0300

    using react and redux

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
+import App from './components/app';
+
+const store = createStore(reducer);//reduser + state
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, document.getElementById('root'));
